ee53f3dafc6a185535120ecf78d396ae714094a7
최승우 <choi0250655@gmail.com>
2026-05-25 00:51:46 +0900

    feat : color system 구현 (#2)

---
+++ lib/core/constants/color.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+abstract class SuccessColor {
+  static const Color light = Color(0xFFEBF9EB);
+  static const Color lightHover = Color(0xFFE1F7E2);
+  static const Color lightActive = Color(0xFFC2EEC2);
+  static const Color normal = Color(0xFF3AC73B);
+  static const Color normalHover = Color(0xFF34B335);
+  static const Color normalActive = Color(0xFF2E9F2F);
+  static const Color dark = Color(0xFF2C952C);
+  static const Color darkHover = Color(0xFF237723);
+  static const Color darkActive = Color(0xFF1A5A1B);
+  static const Color darker = Color(0xFF144615);
+}
+
+abstract class ErrorColor {
+  static const Color light = Color(0xFFFAE9E9);
+  static const Color lightHover = Color(0xFFF8DDDE);
+  static const Color lightActive = Color(0xFFF0B9BB);
+  static const Color normal = Color(0xFFCF1E25);
+  static const Color normalHover = Color(0xFFBA1B21);
+  static const Color normalActive = Color(0xFFA6181E);
+  static const Color dark = Color(0xFF9B171C);
+  static const Color darkHover = Color(0xFF7C1216);
+  static const Color darkActive = Color(0xFF5D0D11);
+  static const Color darker = Color(0xFF480B0D);
+}
+
+abstract class PrimaryColor {
+  static const Color light = Color(0xFFF3FDF3);
+  static const Color lightHover = Color(0xFFEDFBED);
+  static const Color lightActive = Color(0xFFDAF8DA);
+  static const Color normal = Color(0xFF88E788);
+  static const Color normalHover = Color(0xFF7AD07A);
+  static const Color normalActive = Color(0xFF6DB96D);
+  static const Color dark = Color(0xFF66AD66);
+  static const Color darkHover = Color(0xFF528B52);
+  static const Color darkActive = Color(0xFF3D683D);
+  static const Color darker = Color(0xFF305130);
+}
